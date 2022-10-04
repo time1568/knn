29bce54827ebfd40dd5509f21259330494dbb6d3
Thalit Chaimunin <112146489+time1568@users.noreply.github.com>
2022-10-04 23:24:37 +0700

    Update knnnnnnnnnnnnnn.cpp

--- knnnnnnnnnnnnnn.cpp
+++ knnnnnnnnnnnnnn.cpp
@@ -7,17 +7,18 @@ using namespace std;
 struct students
     {
         char name[30];
-        float Ne;
-        float Ni;
-        float Te;
-        float Ti;
-        float Se;
-        float Si;
-        float Fe;
-        float Fi;
+        float at1;
+        float at2;
+        float at3;
+        float at4;
+        float at5;
+        float at6;
+        float at7;
+        float at8;
         char type[10];
         float distance;
     };
+
 struct students student[11];
 
 int main(){
@@ -26,14 +27,13 @@ int main(){
     strcpy(student[0].name, "Key");
     strcpy(student[1].name, "Jake");
     strcpy(student[2].name, "Brondy");
-    strcpy(student[3].name, "Jane");
+    strcpy(student[3].name, "Penny");
     strcpy(student[4].name, "Arthur");
     strcpy(student[5].name, "Kleve");
     strcpy(student[6].name, "Kristy");
     strcpy(student[7].name, "Monrow");
     strcpy(student[8].name, "Evie");
     strcpy(student[9].name, "Victor");
-    
     strcpy(student[10].name, "Sam");
     
     strcpy(student[0].type,"ENFJ");
@@ -46,26 +46,25 @@ int main(){
     strcpy(student[7].type,"INTP");
     strcpy(student[8].type,"ISFJ");
     strcpy(student[9].type,"INTP");
-
     strcpy(student[10].type,"ISTJ");
 
-    student[0].Ne, student[0].Ni,student[0].Te, student[0].Ti,student[0].Se, student[0].Si, student[0].Fe, student[0].Fi = 28.2,22,20,30,27,27,27,20;
-    student[1].Ne, student[1].Ni,student[1].Te, student[1].Ti,student[1].Se, student[1].Si, student[1].Fe, student[1].Fi = 40.2,30,31,30,32,36,37,40;
-    student[2].Ne, student[2].Ni,student[2].Te, student[2].Ti,student[2].Se, student[2].Si, student[2].Fe, student[2].Fi = 41.6,36.6,25,18,27,32,32,40;
-    student[3].Ne, student[3].Ni,student[3].Te, student[3].Ti,student[3].Se, student[3].Si, student[3].Fe, student[3].Fi = 26,25,24,36,33,33,26,21;
-    student[4].Ne, student[4].Ni,student[4].Te, student[4].Ti,student[4].Se, student[4].Si, student[4].Fe, student[4].Fi = 37.4,31.427,38,22,30,43,28;
-    student[5].Ne, student[5].Ni,student[5].Te, student[5].Ti,student[5].Se, student[5].Si, student[5].Fe, student[5].Fi = 28.4,31.2,18,25,21,24,22,18;
-    student[6].Ne, student[6].Ni,student[6].Te, student[6].Ti,student[6].Se, student[6].Si, student[6].Fe, student[6].Fi = 39.4,34,26,24,30,37,30,32;
-    student[7].Ne, student[7].Ni,student[7].Te, student[7].Ti,student[7].Se, student[7].Si, student[7].Fe, student[7].Fi = 29.6,28.6,27,23,28,27,23,30;
-    student[8].Ne, student[8].Ni,student[8].Te, student[8].Ti,student[8].Se, student[8].Si, student[8].Fe, student[8].Fi = 25,25,21,27,21,27,23,24;
-    student[9].Ne, student[9].Ni,student[9].Te, student[9].Ti,student[9].Se, student[9].Si, student[9].Fe, student[9].Fi = 24.8,21.8,27,35,2836,27,22;
+    student[0].at1, student[0].at2,student[0].at3, student[0].at4,student[0].at5, student[0].at6, student[0].at7, student[0].at8 = 28.2,22,20,30,27,27,27,20;
+    student[1].at1, student[1].at2,student[1].at3, student[1].at4,student[1].at5, student[1].at6, student[1].at7, student[1].at8 = 40.2,30,31,30,32,36,37,40;
+    student[2].at1, student[2].at2,student[2].at3, student[2].at4,student[2].at5, student[2].at6, student[2].at7, student[2].at8 = 41.6,36.6,25,18,27,32,32,40;
+    student[3].at1, student[3].at2,student[3].at3, student[3].at4,student[3].at5, student[3].at6, student[3].at7, student[3].at8 = 26,25,24,36,33,33,26,21;
+    student[4].at1, student[4].at2,student[4].at3, student[4].at4,student[4].at5, student[4].at6, student[4].at7, student[4].at8 = 37.4,31.427,38,22,30,43,28;
+    student[5].at1, student[5].at2,student[5].at3, student[5].at4,student[5].at5, student[5].at6, student[5].at7, student[5].at8 = 28.4,31.2,18,25,21,24,22,18;
+    student[6].at1, student[6].at2,student[6].at3, student[6].at4,student[6].at5, student[6].at6, student[6].at7, student[6].at8 = 39.4,34,26,24,30,37,30,32;
+    student[7].at1, student[7].at2,student[7].at3, student[7].at4,student[7].at5, student[7].at6, student[7].at7, student[7].at8 = 29.6,28.6,27,23,28,27,23,30;
+    student[8].at1, student[8].at2,student[8].at3, student[8].at4,student[8].at5, student[8].at6, student[8].at7, student[8].at8 = 25,25,21,27,21,27,23,24;
+    student[9].at1, student[9].at2,student[9].at3, student[9].at4,student[9].at5, student[9].at6, student[9].at7, student[9].at8 = 24.8,21.8,27,35,2836,27,22;
 
-    student[10].Ne, student[10].Ni, student[10].Te, student[10].Ti, student[10].Se, student[10].Si, student[10].Fe, student[10].Fi = 35,27.8,27,40,39,38,41,33;
+    student[10].at1, student[10].at2, student[10].at3, student[10].at4, student[10].at5, student[10].at6, student[10].at7, student[10].at8 = 35,27.8,27,40,39,38,41,33;
     
     for(int i = 0; i<10; i++){
-        student[i].distance = sqrt(pow((student[i].Ne-student[10].Ne), 2)+ pow((student[i].Ni-student[10].Ni), 2)+ pow((student[i].Te-student[10].Te), 2)+
-                                   pow((student[i].Ti-student[10].Ti), 2)+ pow((student[i].Se-student[10].Se), 2)+ pow((student[i].Si-student[10].Si), 2)+
-                                   pow((student[i].Fe-student[10].Fe), 2)+ pow((student[i].Fi-student[10].Fi), 2));
+        student[i].distance = sqrt(pow((student[i].at1-student[10].at1), 2)+ pow((student[i].at2-student[10].at2), 2)+ pow((student[i].at3-student[10].at3), 2)+
+                                   pow((student[i].at4-student[10].at4), 2)+ pow((student[i].at5-student[10].at5), 2)+ pow((student[i].at6-student[10].at6), 2)+
+                                   pow((student[i].at7-student[10].at7), 2)+ pow((student[i].at8-student[10].at8), 2));
         dt[i] = student[i].distance;
     }
     
@@ -143,8 +142,8 @@ int main(){
     Type1[2] = student[index_min3].type[0];
     Type1[3] = student[index_min4].type[0];
     Type1[4] = student[index_min5].type[0];
-    size_t arraySize1 = sizeof(Type1)/sizeof(*Type1);
-    std::sort(Type1, Type1+arraySize1);
+    size_t arraysize1 = sizeof(Type1)/sizeof(*Type1);
+    std::sort(Type1, Type1+arraysize1);
     myType += Type1[3];
 
     Type2[0] = student[index_min1].type[1];
@@ -152,8 +151,8 @@ int main(){
     Type2[2] = student[index_min3].type[1];
     Type2[3] = student[index_min4].type[1];
     Type2[4] = student[index_min5].type[1];
-    size_t arraySize2 = sizeof(Type2)/sizeof(*Type2);
-    std::sort(Type2, Type2+arraySize2);
+    size_t arraysize2 = sizeof(Type2)/sizeof(*Type2);
+    std::sort(Type2, Type2+arraysize2);
     myType += Type2[3];
 
     Type3[0] = student[index_min1].type[2];
@@ -161,8 +160,8 @@ int main(){
     Type3[2] = student[index_min3].type[2];
     Type3[3] = student[index_min4].type[2];
     Type3[4] = student[index_min5].type[2];
-    size_t arraySize3 = sizeof(Type3)/sizeof(*Type3);
-    std::sort(Type3, Type3+arraySize3);
+    size_t arraysize3 = sizeof(Type3)/sizeof(*Type3);
+    std::sort(Type3, Type3+arraysize3);
     myType += Type3[3];
 
     Type4[0] = student[index_min1].type[3];
@@ -170,8 +169,8 @@ int main(){
     Type4[2] = student[index_min3].type[3];
     Type4[3] = student[index_min4].type[3];
     Type4[4] = student[index_min5].type[3];
-    size_t arraySize4 = sizeof(Type4)/sizeof(*Type4);
-    std::sort(Type4, Type4+arraySize4);
+    size_t arraysize4 = sizeof(Type4)/sizeof(*Type4);
+    std::sort(Type4, Type4+arraysize4);
     myType += Type4[3];
     
    cout << "My type should be " << myType << endl;
